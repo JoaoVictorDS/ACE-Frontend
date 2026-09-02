f937083cb0c7076f07be63ca7771ebe035084e76
unknown <j_victor_jv97@hotmail.com>
2026-09-02 12:06:23 -0300

    feat: cria estrutura inicial do frontend

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/login" element={<h1>ACE Frontend</h1>} />
+        </Routes>
+    )
+}
